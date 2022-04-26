b27afaa111ced11449e4e697778544cb74d98310
Steven Rivera <79433173+StevenRJ17@users.noreply.github.com>
2022-04-25 22:55:57 -0500

    Add files via upload

---
+++ Practicas Complementarias/Practica complementaria 1/Ejercicio-3.js
@@ -0,0 +1,13 @@
+/* Ejercicio-3.	
+    Definir un arreglo con sus comidas favoritas, 
+    teniendo como base el objeto del punto anterior. */
+
+     const comidasFavoritas = [
+        "Encebollado",
+        "Pure de papa con carne frita",
+        "Ceviche"
+    ]
+for(let i=0; i<comidasFavoritas.length; i++){
+    console.log(comidasFavoritas[i]);
+}
+    
